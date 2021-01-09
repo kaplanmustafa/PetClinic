57bb8b836b4bf9f077741a695c52428f5ccc3e16
mustafakaplan <mstfakapln@gmail.com>
2021-01-09 20:31:59 +0300

    owner and pet domain calasses

---
+++ src/main/java/com/mustafakaplan/petclinic/model/Owner.java
@@ -0,0 +1,41 @@
+package com.mustafakaplan.petclinic.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Owner {
+
+	private Long id;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private Set<Pet> pets = new HashSet<>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
+}

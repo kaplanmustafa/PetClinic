a40c0ba7af954b56c1af3e14d6d0f96c5e162d42
mustafakaplan <mstfakapln@gmail.com>
2021-01-10 14:42:00 +0300

    controlle and static web resource

---
+++ src/main/java/com/mustafakaplan/petclinic/web/PetClinicController.java
@@ -0,0 +1,16 @@
+package com.mustafakaplan.petclinic.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PetClinicController {
+
+	@RequestMapping("/pcs")
+	@ResponseBody
+	public String welcome() {
+		
+		return "Welcome to PetClinic World!";
+	}
+}

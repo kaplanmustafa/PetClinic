64a33b4d2c99f00ef7bc2568bcff772958ac6971
mustafakaplan <mstfakapln@gmail.com>
2021-01-10 17:49:17 +0300

    custom servlet

--- src/main/java/com/mustafakaplan/petclinic/PetClinicApplication.java
+++ src/main/java/com/mustafakaplan/petclinic/PetClinicApplication.java
@@ -3,7 +3,9 @@ package com.mustafakaplan.petclinic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 @EnableConfigurationProperties(value = PetClinicProperties.class)
 public class PetClinicApplication {
